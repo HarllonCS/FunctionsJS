7f405615891b07edeb2ce6696f5f0013b52870e3
Harllon <96022743+HarllonCS@users.noreply.github.com>
2021-12-27 11:04:53 -0300

    Function 2

--- script.js
+++ script.js
@@ -1,32 +1,62 @@
+p = document.createElement('p')
+
 numero = document.querySelector('#iNum')
 res = document.querySelector('.res')
-lista = document.querySelector('#sel')
+ls = document.querySelector('#sel')
 vetorNums = []
 
 function adicionar() {
     if (numero.value.length > 0) {
 
-        let num = Number(numero.value)
+        let num = numero.value
 
         if ((num >= 1 && num <= 100) && vetorNums.indexOf(num) == -1) {
 
-            opt = document.createElement('option')
-            lista.appendChild(opt)
+            let opt = document.createElement('option')
             
             vetorNums.push(num)
+
+            ls.appendChild(opt)
             
             opt.text = `Adicionado: ${num}`
+
+            p.innerHTML = ''
 
         } else if (num < 1 || num > 100) {
             alert('O número tem que ser entre 1 à 100.')
         } else {
-            alert(`Número "${num}" já está na lista`)
+            alert(`Número "${num}" já está na lista.`)
         }
+
+        numero.focus()
+
     } else {
         alert('Insira um número.')
     }
 }
 
-function mostrar(vetorNums) {
+function mostrar() {
+
+    let maiorValor = vetorNums[0]
+    let menosValor = vetorNums[0]
     
+    for (let pos in vetorNums) {
+        
+        if (vetorNums[pos] > maiorValor) {
+            maiorValor = vetorNums[pos]
+        } else {
+            menosValor = vetorNums[pos]
+        }
+    }
+
+    if (vetorNums.length > 0) {
+        res.appendChild(p)
+        
+        p.innerHTML = `Total de valores: <span style='color: red;'>${vetorNums.length}<span>`
+        p.innerHTML += `<br>Maior Valor: ${maiorValor}`
+        p.innerHTML += `<br>Menor Valor: ${menosValor}`
+
+    } else {
+        alert('Adicione os valores antes de clicar nesse botão.')
+    }
 }
